146677eb97661e7fe259c2530833bcbf00a3eab6
Ramon Andrade Sousa Carvalho <ramon.andrade@live.com>
2024-07-08 19:43:33 -0300

    Disable Pencil's conceal

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -31,6 +31,7 @@ vim_pencil_config = function(plugin, opts)
   vim_pencil_autocmds(plugin, opts)
   vim.g["pencil#textwidth"] = 79
   vim.g["pencil#cursorwrap"] = 1
+  vim.g["pencil#conceallevel"] = 0
   vim.g["pencil#autoformat"] = 0
   vim.g["pencil#joinspace"] = 0
 end
