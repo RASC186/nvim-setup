708f6dacb9974422d6fef7d040e1fe2e96c961da
Ramon Andrade Sousa Carvalho <ramon.andrade@live.com>
2024-07-14 01:13:24 -0300

    Refactor setup and refresh scripts

--- refresh.sh
+++ refresh.sh
@@ -4,11 +4,16 @@
 
 echo -e "\n\033[32mCopying Neovin settings to ./config directory...\033[0m\n"
 
-rm -r "${HOME}/.config/nvim/"
+if [ -d "${HOME}/.config/nvim/" ] ; then
+
+  rm -rf "${HOME}/.config/nvim/"
+
+fi
+
 cp -R "./nvim" "${HOME}/.config/"
 
 if [[ $? = '0' ]] ; then
-  echo -e "Files successfully copied!"
+  echo -e "Files successfully copied!\n"
 else
-  echo -e "\033[0;31mNeovim settings copy failed!\033[0m"
+  echo -e "\033[0;31mNeovim settings copy failed!\033[0m\n"
 fi
--- setup.sh
+++ setup.sh
@@ -1,183 +1,72 @@
 #!/bin/bash
 
-##Set the Ctrl-C trap
+## Set the Ctrl-C trap
 
 trap "echo -e \"\nTerminated by user\" ; trap - SIGINT ; exit" SIGINT
 
-##Remove Vim
-
-echo -e "\n\033[32mUninstalling Vim\033[0m\n"
-
-sudo apt remove vim
-
-##Update and upgrade packages
+## Update and upgrade packages
 
 echo -e "\n\033[32mUpdating and upgrading packages\033[0m\n"
 
 sudo apt update
 sudo apt upgrade -y
 
-##Install Neovim dependencies
+## Install Neovim dependencies
 
 echo -e "\n\033[32mInstalling Neovim dependencies\033[0m\n"
 
-sudo apt install -y snap snapd git curl unzip luarocks npm golang gdb ripgrep \
-  bear yarn latexmk default-jdk texlive-full xclip
+sudo apt install -y git
+sudo apt install -y wget
+sudo apt install -y unzip
+sudo apt install -y xclip
 
-sudo curl https://sh.rustup.rs -sSf | sh
+sudo apt install -y bear
+sudo apt install -y fuse
+sudo apt install -y ripgrep
 
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+sudo apt install -y gdb make cmake  
+sudo apt install -y lua5.4 luarocks
+sudo apt install -y python3.11 python3.11-venv python3-pip
+sudo apt install -y rustc cargo
+sudo apt install -y golang
+sudo apt install -y openjdk-17-jdk 
+sudo apt install -y latexmk texlive-full 
 
-sudo nvm install 20
-
-readarray -t lua_versions < \
-  <(apt list lua* | grep -oP "lua[\d\.]+(?=/)" | sort -r)
-
-readarray -t python_versions < \
-  <(apt list python* | grep -oP "python[\d\.]+(?=/)" | sort -r)
-
-readarray -t python_venv_versions < \
-  <(apt list python*-venv | grep -oP "python[\d\.]+-venv(?=/)" | sort -r)
-
-sudo apt install -y "${lua_versions[0]}"
-sudo apt install -y "${python_versions[0]}"
-sudo apt install -y "${python_venv_versions[0]}"
-
-##Refresh Snap
-
-PATH=$PATH:/snap/bin/
-
-echo -e "\n\033[32mRefreshing Snap\033[0m\n"
-
-sudo snap refresh
-
-##Install Neovim
+## Install Neovim
 
 echo -e "\n\033[32mInstalling Neovim\033[0m\n"
 
-sudo snap install nvim --classic
+if [ -f /usr/bin/nvim ] ; then
 
-##Create an alias for Neovim
+  echo "Neovim is already installed."
 
-echo -e "\n\033[32mCreating an alias for Neovim\033[0m\n"
-
-nvim_path=$(which nvim)
-
-echo "alias vim=\"${nvim_path}\"" > .bashrc
-
-localFileContent=$(cat ./.bashrc)
-homeFileContent=$(cat ~/.bashrc)
-
-if ! [[ ${homeFileContent} = *${localFileContent}* ]] ; then
-	cat ./.bashrc >> ~/.bashrc
-fi
-
-cat ./.bashrc
-
-##Create an symbolic link for Neovim 
-
-echo -e "\n\033[32mCreating a symbolic link for Neovim\033[0m\n"
-
-sudo ln -s "${nvim_path}" "/usr/bin/nvim"
-
-##Install tree-sitter-cli
-
-echo -e "\n\033[32mInstalling tree-sitter-cli\033[0m\n"
-
-sudo cargo install tree-sitter-cli
-
-sudo mv /root/.cargo/bin/tree-sitter /usr/bin/
-
-##Install a Nerd Font
-
-echo -e "\n\033[32mInstalling a nerd font\033[0m\n"
-
-echo -e "Scanning /ryanoasis/nerd-fonts repo..."
-
-readarray -t endpoints 	< \
- 	<(curl -s 'https://github.com/ryanoasis/nerd-fonts/releases' |\
-  grep -oP '\"/ryanoasis/nerd-fonts/releases/tag/[\w\d\/\.]+\"')
-
-latest_version="$(echo "${endpoints[0]}" | grep -oP '(?<=tag/)v[\d\.]+')"
-
-if [[ -z ${endpoints[*]} ]] ; then
-  echo -e "\033[0;31mLatest version not found.\033[0m"
-  exit 
 else
-  echo -e "Latest version found: ${latest_version}"
-fi
 
-echo -e "Getting nerd fonts download links..."
+  sudo wget -O "/opt/nvim.tar.gz" \
+    "https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-linux64.tar.gz"
 
-readarray -t download_links < \
-	<(curl -s "https://github.com/ryanoasis/nerd-fonts/releases/expanded_assets/${latest_version}" |\
-  grep -oP "\"/ryanoasis/nerd-fonts/releases/download/${latest_version}/[\\w\\d\\.]+\"")
+  sudo tar -xzvf "/opt/nvim.tar.gz" --directory "/opt/"
 
-if [[ -z ${download_links[*]} ]] ; then
-  echo -e "\033[0;31mLinks not found.\033[0m"
-  exit
-else
-  echo "Fonts found:"
-fi
+  sudo rm -f "/opt/nvim.tar.gz"
 
-declare -A fonts
-for link in "${download_links[@]}" ; do
-	font_name=$(echo "${link}" | grep -oP '(?<=/)[\w\d]+(?=\.[\w\d\.]+")')
-	fonts["${font_name}"]="$(echo "${link}" | tr -d '"')"
-done
+  sudo ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
 
-keys=${!fonts[*]}
-sorted_keys=$(echo -e "${keys// /\\n}" | sort)
-
-PS3="Insert font number: "
-select font in ${sorted_keys} ; do
-	
-	if [[ -z "${fonts[${font}]}" ]] ; then
-		echo -e "\033[0;31mInvalid input!\033[0m"
-	else	
-	  selected_font="https://github.com${fonts[${font}]}"
-    break
-	fi
-
-done
-
-downloads_dirpath="/tmp/nvim-nerdfonts/"
-compacted_filename="$(echo "${selected_font}" | grep -oP '(?<=/)[\w\d\.]+(?=$)')"
-compacted_filepath="${downloads_dirpath}${compacted_filename}"
-
-echo -e "Downloading ${selected_font}..."
-
-mkdir "${downloads_dirpath}"
-wget_error="$(wget -O "${compacted_filepath}" "${selected_font}" 2>&1 | grep -oP 'ERROR[\s\S]+')"
-wait $!
-
-if [[ ${wget_error} ]] ; then
-  echo -e "\033[0;31mDownload failed! ${wget_error}\033[0m"
-  exit
-fi
-
-compacted_file_extention="$(echo "${selected_font}" | grep -oP '(?<=\.)[\w\d\.]+(?=$)')"
-fonts_dirpath="${HOME}/.local/share/fonts/$(echo "${compacted_filename}" | grep -oP '[\w\d]+(?=\.)')"
-
-mkdir "${HOME}/.local/share/fonts/"
-
-if [[ ${compacted_file_extention} = 'tar' ]] ; then
-	tar -xf "${compacted_filepath}" -C "${fonts_dirpath}"
-elif [[ ${compacted_file_extention} = 'tar.'* ]] ; then
-	tar -xf "${compacted_filepath}" -C "${fonts_dirpath}"
-elif [[ ${compacted_file_extention} = 'zip' ]] ; then
-	unzip "${compacted_filepath}" -d "${fonts_dirpath}"
 fi
 
 ##Copy Neovim settings to ~/.config directory
 
 echo -e "\n\033[32mCopying Neovin settings to ./config directory...\033[0m\n"
 
-rm -r "${HOME}/.config/nvim/"
+if [ -d "${HOME}/.config/nvim/" ] ; then
+
+  rm -rf "${HOME}/.config/nvim/"
+
+fi
+
 cp -R "./nvim" "${HOME}/.config/"
 
 if [[ $? = '0' ]] ; then
-  echo -e "Files successfully copied!"
+  echo -e "Files successfully copied!\n"
 else
-  echo -e "\033[0;31mNeovim settings copy failed!\033[0m"
+  echo -e "\033[0;31mNeovim settings copy failed!\033[0m\n"
 fi
