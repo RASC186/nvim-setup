4052f796034c36c843c0023ff1b3d114703e96bd
Ramon Andrade Sousa Carvalho <ramon.andrade@live.com>
2024-08-06 22:12:23 -0300

    Add keymap to quit all

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -7,6 +7,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>q", ":q<CR>", { silent = true })
+vim.keymap.set("n", "<leader>Q", ":qa<CR>", { silent = true })
 vim.keymap.set("n", "<leader><Right>", ":bnext<CR>", { silent = true })
 vim.keymap.set("n", "<leader><Left>", ":bprevious<CR>", { silent = true })
 
